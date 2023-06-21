25bfc9988b8466dee65dbb5f07cb2594884cca9e
quannguyen2153 <nmquan213@clc.fitus.edu.vn>
2023-06-21 17:01:24 +0700

    Fix bug appear when no internet connection

--- Eivom/app/src/main/java/powerrangers/eivom/feature_movie/domain/utility/Resource.kt
+++ Eivom/app/src/main/java/powerrangers/eivom/feature_movie/domain/utility/Resource.kt
@@ -6,33 +6,13 @@ sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Loading<T>(data: T? = null) : Resource<T>(data)
 }
 
-object ResourceErrorMessage {
-    const val UNKNOWN = "Unknown error"
-
-    const val RESOURCE_ERROR = "Resource error"
-
-    const val ADD_LIST = "Adding Resource<List> error"
-
-    const val CONVERT_MOVIELIST_TO_MOVIELISTITEMS = "Converting Resource<MovieList> to Resource<MovieListItems> error"
-    const val CONVERT_MOVIEINFORMATION_TO_MOVIEITEM = "Converting Resource<MovieInformation> to Resource<MovieItem> error"
-
-    const val GET_MOVIELIST = "Getting Resource<MOVIELIST> error"
-    const val GET_MOVIEINFORMATION = "Getting Resource<MOVIEINFORMATION> error"
-
-    const val MOVIELIST_END = "End of movie list error"
-    const val LOAD_MOVIELIST = "Loading movie list error"
-}
-
 fun <T> Resource<List<T>>.addList(resource: Resource<List<T>>): Resource<List<T>> {
     return try {
-        val list = (this.data ?: emptyList()) + (resource.data ?: emptyList())
-        if (this is Resource.Error || resource is Resource.Error)
-            Resource.Error(data = list, message = ResourceErrorMessage.RESOURCE_ERROR)
-        else
-            Resource.Success(data = list)
+        val list = (this.data ?: emptyList()) + resource.data!!
+        Resource.Success(data = list)
     }
     catch (e: Exception) {
-        Resource.Error(data = this.data, message = ResourceErrorMessage.ADD_LIST)
+        Resource.Error(data = this.data, message = e.message ?: resource.message ?: ResourceErrorMessage.ADD_LIST)
     }
 }
 
@@ -48,4 +28,19 @@ fun <T> Resource<T>.toError(data: T? = null, message: String): Resource.Error<T>
         Resource.Error(data = data, message = message)
     else
         Resource.Error(data = this.data, message = message)
+}
+
+object ResourceErrorMessage {
+    const val UNKNOWN = "Unknown error"
+
+    const val ADD_LIST = "Adding Resource<List> error"
+
+    const val CONVERT_MOVIELIST_TO_MOVIELISTITEMS = "Converting Resource<MovieList> to Resource<MovieListItems> error"
+    const val CONVERT_MOVIEINFORMATION_TO_MOVIEITEM = "Converting Resource<MovieInformation> to Resource<MovieItem> error"
+
+    const val GET_MOVIELIST = "Getting Resource<MOVIELIST> error"
+    const val GET_MOVIEINFORMATION = "Getting Resource<MOVIEINFORMATION> error"
+
+    const val MOVIELIST_END = "End of movie list error"
+    const val LOAD_MOVIELIST = "Loading movie list error"
 }
