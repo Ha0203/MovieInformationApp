e74d3157e37c74f661995c8cc002e4468032c251
Loi-Tap-Lam-Dev <npminhbaobao2@gmail.com>
2023-08-15 17:01:05 +0700

    Add Icon note and watched

--- Eivom/app/src/main/java/powerrangers/eivom/feature_movie/ui/movie_detail/MovieDetailScreen.kt
+++ Eivom/app/src/main/java/powerrangers/eivom/feature_movie/ui/movie_detail/MovieDetailScreen.kt
@@ -231,6 +231,29 @@ fun TopButton(
             )
         }
         Spacer(modifier = Modifier.weight(1f))
+        // Watched
+        Icon(
+            painter = painterResource(id = R.drawable.ic_note),
+            contentDescription = null,
+            modifier = Modifier
+                .size(60.dp)
+                .scale(-1f, 1f) // Flip horizontally
+                .padding(5.dp)
+            ,
+            tint = Color.Black
+        )
+
+        // Watched
+//        Icon(
+//            painter = painterResource(id = R.drawable.ic_watched),
+//            contentDescription = null,
+//            modifier = Modifier
+//                .size(60.dp)
+//                .scale(-1f, 1f) // Flip horizontally
+//                .padding(5.dp)
+//            ,
+//            tint = Color.Black
+//        )
         // Favorite
         IconButton(onClick = { /*ADD Function*/  }) {
             Icon(
